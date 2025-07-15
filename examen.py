0c3810dd15c8751087dc3b74eef6add130849485
catalina261 <ca.hinojosal@duocuc.cl>
2025-07-15 13:40:39 -0400

    Add files via upload

---
+++ examen.py
@@ -0,0 +1,55 @@
+"tienda pybooks"
+#productos = {modelo: [marca, pantalla, RAM, disco, GB de DD, procesador, videos], ...}
+#stock = {modelo: [precio, stock], ...}
+"menu prinicpal"
+print("1 Stock marca")
+print("2 Busqueda por precio")
+print("3 Eliminar producto")
+print("4 Salir")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
